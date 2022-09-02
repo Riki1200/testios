9147cff5e6cb0ef9349d2742461010a98b956467
Romeo <45272844+Riki1200@users.noreply.github.com>
2022-09-01 21:59:20 -0400

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Romeo Betances on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
